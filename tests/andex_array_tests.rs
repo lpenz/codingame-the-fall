23884b5b465f7e0c453844f5452e9e8cbc6bab82
Leandro Lisboa Penz <lpenz@lpenz.org>
2022-06-25 16:58:27 +0100

    Squashed 'external/andex/' content from commit e9a878d
    git-subtree-dir: external/andex
    git-subtree-split: e9a878d381bb4b8b2ed0c0ed3c6f763b9eb41485

---
+++ tests/andex_array_tests.rs
@@ -0,0 +1,89 @@
+// Copyright (C) 2021 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use andex::*;
+
+use std::convert::TryFrom;
+
+use anyhow::Result;
+
+/* Tests for arrays: */
+
+pub struct MyIdxInner;
+type MyIdx = Andex<MyIdxInner, 12>;
+
+type MyArray = AndexableArray<MyIdx, u32, { MyIdx::SIZE }>;
+
+type MyArray2 = andex::andex_array!(MyIdx, u32);
+
+pub struct NoTraits {}
+type _MyArrayNoTraits = AndexableArray<MyIdx, NoTraits, { MyIdx::SIZE }>;
+
+#[test]
+fn test_myarr() -> Result<()> {
+    let mut m = MyArray::default();
+    m[MyIdx::new::<2>()] = 5;
+    m[MyIdx::try_from(2)?] = 5;
+    for (num, i) in MyIdx::iter().enumerate() {
+        m[i] = num as u32 + 20;
+    }
+    for (num, i) in MyIdx::iter().enumerate() {
+        assert_eq!(m[i], num as u32 + 20);
+    }
+    for (num, i) in MyIdx::iter().enumerate() {
+        m[&i] = num as u32 + 30;
+    }
+    for (num, i) in MyIdx::iter().enumerate() {
+        assert_eq!(m[&i], num as u32 + 30);
+    }
+    let _ = MyIdx::iter().map(|i| i);
+    println!("{:?}", m);
+    Ok(())
+}
+
+#[test]
+fn test_conversions() -> Result<()> {
+    let mut myarray1 = MyArray::from([3; 12]);
+    let array1 = myarray1.as_mut();
+    for i in 0..12 {
+        array1[i] = i as u32;
+    }
+    for i in MyIdx::iter() {
+        assert_eq!(myarray1[i], usize::from(i) as u32);
+    }
+    let array2 = <[u32; 12]>::from(&myarray1);
+    assert_eq!(&array2, myarray1.as_ref());
+    let array3 = <[u32; 12]>::from(myarray1);
+    assert_eq!(array3, array2);
+    let myarray3 = array3.iter().collect::<MyArray>();
+    assert_eq!(myarray3.as_ref(), &array3);
+    let myarray4 = array3.iter().cloned().collect::<MyArray>();
+    assert_eq!(myarray4.as_ref(), &array3);
+    let _myarray5 = myarray4.clone();
+    let _myarray6 = *&myarray4;
+    Ok(())
+}
+
+#[test]
+fn test_iter() -> Result<()> {
+    let mut myarray = MyArray2::from([3; 12]);
+    for item in &mut myarray {
+        *item = 5;
+    }
+    for item in &myarray {
+        assert_eq!(*item, 5);
+    }
+    assert!(myarray.iter().all(|i| *i == 5));
+    Ok(())
+}
+
+// Enable when we start using a version with array.into_iter() (Rust 2021)
+// #[test]
+// fn test_intoiter() -> Result<()> {
+//     let myarray = MyArray2::from([3; 12]);
+//     for item in myarray {
+//         assert_eq!(item, 3);
+//     }
+//     Ok(())
+// }
---
+++ tests/andex_tests.rs
@@ -0,0 +1,113 @@
+// Copyright (C) 2021 Leandro Lisboa Penz <lpenz@lpenz.org>
+// This file is subject to the terms and conditions defined in
+// file 'LICENSE', which is part of this source code package.
+
+use andex::*;
+
+use std::convert::TryFrom;
+
+use anyhow::Result;
+
+/* Basic tests */
+
+enum Marker {}
+type C = Andex<Marker, 3>;
+
+#[test]
+fn test_basic() -> Result<()> {
+    for i in C::iter() {
+        assert!(usize::from(i) < 3_usize);
+    }
+    let is = C::iter().map(usize::from).collect::<Vec<_>>();
+    assert_eq!(is, vec![0, 1, 2]);
+    let i = C::default();
+    assert_eq!(usize::from(i), 0_usize);
+    const J: C = C::new::<2>();
+    assert_eq!(usize::from(J), 2_usize);
+    let k: C = C::new::<1>();
+    assert_eq!(usize::from(k), 1_usize);
+    assert_eq!(usize::from(C::try_from(2)?), 2);
+    assert!(C::try_from(3).is_err());
+    let u = k.clone();
+    assert_eq!(u, k);
+    Ok(())
+}
+
+#[test]
+#[should_panic]
+fn test_oob1() {
+    let u: C = C::new::<5>();
+    assert_eq!(usize::from(u), 5_usize);
+}
+
+// // This doesn't compile, which is correct:
+// #[test]
+// #[should_panic]
+// fn test_oob2() {
+//     const u: C = C::new::<5>();
+//     assert_eq!(usize::from(u), 5_usize);
+// }
+
+#[test]
+fn test_try_from() {
+    let result = C::try_from(5);
+    assert!(result.is_err());
+    if let Err(ref err) = result {
+        println!("{}, {:?}", err, err);
+        let _ = err.clone();
+    }
+}
+
+#[test]
+fn test_parse() {
+    let c = "0".parse::<C>();
+    assert!(c.is_ok());
+    assert_eq!(usize::from(c.unwrap()), 0_usize);
+    let c = "asdf".parse::<C>();
+    if let Err(ref err) = c {
+        println!("{}", err);
+    }
+    assert!(c.is_err());
+    let c = "4".parse::<C>();
+    if let Err(ref err) = c {
+        println!("{}", err);
+    }
+    assert!(c.is_err());
+}
+
+#[test]
+fn test_pair() {
+    let f: C = C::FIRST;
+    assert_eq!(f.pair(), C::LAST);
+}
+
+/* Iterator */
+
+#[test]
+fn test_iterator() {
+    let mut it = C::iter();
+    println!("{:?}", it);
+    let first = it.next().unwrap();
+    assert_eq!(usize::from(first), 0);
+    let second = it.next().unwrap();
+    assert_eq!(usize::from(second), 1);
+    assert!(first < second);
+    assert!(first <= first);
+    assert_eq!(usize::from(it.next().unwrap()), 2);
+    assert!(it.next().is_none());
+    assert!(it.next().is_none());
+}
+
+/* Test automatic traits */
+
+#[test]
+fn test_send() {
+    fn assert_send<T: Send>() {}
+    assert_send::<C>();
+}
+
+#[test]
+fn test_sync() {
+    fn assert_sync<T: Sync>() {}
+    assert_sync::<C>();
+}
